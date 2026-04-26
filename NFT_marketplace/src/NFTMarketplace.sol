3bc48e9203ffa68d5489dce849cf47aa27a2e0d4
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 23:04:20 +0000

    NFT Marketplace fix 64: Add testFuzz_ListAndBuy fuzz test

--- NFT_marketplace/src/NFTMarketplace.sol
+++ NFT_marketplace/src/NFTMarketplace.sol
@@ -467,3 +467,4 @@ contract NFTMarketplace is INFTMarketplace {
 // NFT Marketplace fix 61: Add test_WithdrawFees_Success coverage
 // NFT Marketplace fix 62: Add test_SetFee_Success coverage
 // NFT Marketplace fix 63: Add test_TwoStepOwnership coverage
+// NFT Marketplace fix 64: Add testFuzz_ListAndBuy fuzz test
