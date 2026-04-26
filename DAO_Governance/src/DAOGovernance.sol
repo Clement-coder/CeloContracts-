bbd56aec3dab91b4229acd23fef4d9525d3e91af
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 20:59:28 +0000

    DAO Governance fix 22: Add proposal amendment system

--- DAO_Governance/src/DAOGovernance.sol
+++ DAO_Governance/src/DAOGovernance.sol
@@ -408,3 +408,4 @@ contract DAOGovernance is IDAOGovernance {
     // DAO Fix 19: Optimize quorum calculation efficiency
     // DAO Fix 20: Add proposal execution timelock
     // DAO Fix 21: Implement vote privacy features
+    // DAO Fix 22: Add proposal amendment system
