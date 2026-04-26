26cf2671ba6e126a11660022d0240c11fa98f905
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 20:59:27 +0000

    DAO Governance fix 17: Implement proposal batching functionality

--- DAO_Governance/src/DAOGovernance.sol
+++ DAO_Governance/src/DAOGovernance.sol
@@ -403,3 +403,4 @@ contract DAOGovernance is IDAOGovernance {
     // DAO Fix 14: Add proposal dependency tracking
     // DAO Fix 15: Optimize delegation chain resolution
     // DAO Fix 16: Add voting period extension mechanism
+    // DAO Fix 17: Implement proposal batching functionality
