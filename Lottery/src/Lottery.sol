93755a9df6c1f5b711fe2c0ea327abf932131043
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 22:43:15 +0000

    Lottery fix 53: Add test_DrawWinner_SingleEntryAlwaysWins

--- Lottery/src/Lottery.sol
+++ Lottery/src/Lottery.sol
@@ -354,3 +354,4 @@ contract Lottery is ILottery {
 // Lottery fix 50: Add test_Invariant_AccruedFeesNeverExceedsBalance
 // Lottery fix 51: Add test_MultipleRounds_IndependentPots
 // Lottery fix 52: Add test_MultipleRounds_TicketsNotCarryOver
+// Lottery fix 53: Add test_DrawWinner_SingleEntryAlwaysWins
