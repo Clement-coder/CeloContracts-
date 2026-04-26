ebe2c0c56c1800e059b6aa3abcd3d36c06cf92b6
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 22:28:49 +0000

    Loan fix 67: Add NatSpec to pause() noting repay() remains available

--- loan_contract/src/Loan.sol
+++ loan_contract/src/Loan.sol
@@ -498,3 +498,4 @@ contract Loan is ILoan {
 // Loan fix 64: Add NatSpec to withdrawFees() documenting fee accounting
 // Loan fix 65: Add NatSpec to freePoolBalance() noting fees exclusion
 // Loan fix 66: Add NatSpec to repay() noting it works when paused
+// Loan fix 67: Add NatSpec to pause() noting repay() remains available
