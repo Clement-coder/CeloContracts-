b46a6304326b6fafd7717e8f2f810b13ca3bccbf
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 21:14:07 +0000

    ERC1155 fix 25: Add token insurance features

--- ERC1155/src/ERC1155.sol
+++ ERC1155/src/ERC1155.sol
@@ -298,3 +298,4 @@ interface IERC1155Receiver {
     // ERC1155 Fix 22: Implement cross-chain compatibility
     // ERC1155 Fix 23: Add token rental system
     // ERC1155 Fix 24: Optimize contract initialization
+    // ERC1155 Fix 25: Add token insurance features
