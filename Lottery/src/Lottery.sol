2249b118b842162657caa3cd76423401f6942047
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 22:42:43 +0000

    Lottery fix 38: Add test_WithdrawFees_RevertNoFees (zero fees case)

--- Lottery/src/Lottery.sol
+++ Lottery/src/Lottery.sol
@@ -339,3 +339,4 @@ contract Lottery is ILottery {
 // Lottery fix 35: Add test_BuyTicketsForMultiple_RevertExceedsMax
 // Lottery fix 36: Add test_BuyTicketsForMultiple_RevertWhenPaused
 // Lottery fix 37: Add test_BuyTicketsForMultiple_FeeDeducted
+// Lottery fix 38: Add test_WithdrawFees_RevertNoFees (zero fees case)
