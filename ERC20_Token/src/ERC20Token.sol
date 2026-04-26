86bc1f1a8f958c87c26fe6d3775b3340b8912e60
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 21:06:46 +0000

    ERC20 Token fix 39: Optimize snapshot query performance

--- ERC20_Token/src/ERC20Token.sol
+++ ERC20_Token/src/ERC20Token.sol
@@ -364,3 +364,4 @@ contract ERC20Token is IERC20Token {
     // ERC20 Fix 36: Add token bridge compatibility
     // ERC20 Fix 37: Implement supply adjustment controls
     // ERC20 Fix 38: Add emergency freeze functionality
+    // ERC20 Fix 39: Optimize snapshot query performance
