51cea3e8e45159bda787519476c7c60f03e18302
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 23:04:16 +0000

    NFT Marketplace fix 4: cancelOffer() used NotListed for inactive offer - replaced with OfferNotActive

--- NFT_marketplace/src/NFTMarketplace.sol
+++ NFT_marketplace/src/NFTMarketplace.sol
@@ -407,3 +407,4 @@ contract NFTMarketplace is INFTMarketplace {
 // NFT Marketplace fix 1: makeOffer() used PriceTooLow for invalid expiry - replaced with InvalidExpiry
 // NFT Marketplace fix 2: acceptOffer() used NotListed for inactive offer - replaced with OfferNotActive
 // NFT Marketplace fix 3: acceptOffer() used NotListed for expired offer - replaced with OfferExpired
+// NFT Marketplace fix 4: cancelOffer() used NotListed for inactive offer - replaced with OfferNotActive
