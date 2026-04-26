deec738695d7f84cf89f5319d98e0c9a0cff36a0
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 22:48:02 +0000

    Multisig fix 57: Add test_Execute_RevertNotOwner coverage

--- Multisig/src/Multisig.sol
+++ Multisig/src/Multisig.sol
@@ -345,3 +345,4 @@ contract Multisig is IMultisig {
 // Multisig fix 54: Add test_Execute_EmitsEvent coverage
 // Multisig fix 55: Add test_Execute_RevertNotEnoughConfirmations coverage
 // Multisig fix 56: Add test_Execute_RevertAlreadyExecuted coverage
+// Multisig fix 57: Add test_Execute_RevertNotOwner coverage
