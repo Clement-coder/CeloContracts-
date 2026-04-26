00091457f838256750321e71547fb1f6684a8783
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 22:28:47 +0000

    Loan fix 34: Add test_Repay_WorksWhenPaused - repay must succeed even when paused

--- loan_contract/src/Loan.sol
+++ loan_contract/src/Loan.sol
@@ -465,3 +465,4 @@ contract Loan is ILoan {
 // Loan fix 31: Add test_Constructor_MaxRateAllowed boundary test
 // Loan fix 32: Add test_Borrow_WithOriginationFee verifying fee deduction
 // Loan fix 33: Add test_Borrow_FreePoolExcludesFees invariant test
+// Loan fix 34: Add test_Repay_WorksWhenPaused - repay must succeed even when paused
