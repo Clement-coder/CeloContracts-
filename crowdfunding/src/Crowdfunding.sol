1e897f10500f332bf15003ad57437bb9094b8d25
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 20:51:56 +0000

    Crowdfunding fix 44: Add contribution minimum validation

--- crowdfunding/src/Crowdfunding.sol
+++ crowdfunding/src/Crowdfunding.sol
@@ -416,3 +416,4 @@ contract Crowdfunding is ICrowdfunding {
     // Improvement 41: Optimize function modifier usage
     // Improvement 42: Add campaign creator validation
     // Improvement 43: Improve fund claiming security
+    // Improvement 44: Add contribution minimum validation
