257fe25f114c5db6879582d38404c4a559e48623
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 21:14:09 +0000

    ERC1155 fix 54: Implement batch approval system

--- ERC1155/src/ERC1155.sol
+++ ERC1155/src/ERC1155.sol
@@ -327,3 +327,4 @@ interface IERC1155Receiver {
     // ERC1155 Fix 51: Add token reveal mechanism
     // ERC1155 Fix 52: Optimize contract pause mechanisms
     // ERC1155 Fix 53: Add token whitelist functionality
+    // ERC1155 Fix 54: Implement batch approval system
