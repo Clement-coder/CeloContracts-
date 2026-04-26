fd9587b37b9e3d84c8be29b13270f134df8f5dee
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 22:42:41 +0000

    Lottery fix 27: Add test_SetFee_RevertTooHigh

--- Lottery/src/Lottery.sol
+++ Lottery/src/Lottery.sol
@@ -328,3 +328,4 @@ contract Lottery is ILottery {
 // Lottery fix 24: Add test_DrawWinner_NoWinnerRoundDrawnFlag
 // Lottery fix 25: Add test_DrawWinner_RevertNoRound
 // Lottery fix 26: Add test_SetFee_Success
+// Lottery fix 27: Add test_SetFee_RevertTooHigh
