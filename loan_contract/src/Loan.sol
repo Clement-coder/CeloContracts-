2afabd044b09a5adad869a0e5ab07adfd2d6d288
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 22:28:46 +0000

    Loan fix 18: Add getLoanInfo() to ILoan interface

--- loan_contract/src/Loan.sol
+++ loan_contract/src/Loan.sol
@@ -449,3 +449,4 @@ contract Loan is ILoan {
 // Loan fix 15: Add EXTENSION_DURATION constant (7 days per extension)
 // Loan fix 16: Add extensionCount field to LoanRecord struct
 // Loan fix 17: Add getLoanInfo() view function exposing full loan details including extensionCount
+// Loan fix 18: Add getLoanInfo() to ILoan interface
