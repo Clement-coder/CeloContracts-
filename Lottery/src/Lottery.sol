f2eaa20e7c2c91fa978764db4eb666aa9ab06e8e
Clement Raymond <chinexzy37@gmail.com>
2026-04-26 22:43:17 +0000

    Lottery fix 70: Add carol deal in setUp for multi-player tests

--- Lottery/src/Lottery.sol
+++ Lottery/src/Lottery.sol
@@ -371,3 +371,4 @@ contract Lottery is ILottery {
 // Lottery fix 67: Add test_GetRound_WinnerZeroBeforeDraw
 // Lottery fix 68: Add test_GetRound_DrawnFalseBeforeDraw
 // Lottery fix 69: Remove stale optimization comments from test file
+// Lottery fix 70: Add carol deal in setUp for multi-player tests
